b83acb9432ba2670f07f11f1f7e776d682508b7a
Alex Miroshnyk <miroshnik.alex@gmail.com>
2016-12-15 11:03:21 +0200

    template has been added

--- cookbooks/www-server-install/recipes/default.rb
+++ cookbooks/www-server-install/recipes/default.rb
@@ -3,3 +3,26 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+package 'Install Apache' do
+    case node[:platform]
+    when 'redhat', 'centos', 'fedora'
+	package_name 'httpd'
+    when 'ubuntu', 'debian'
+	package_name 'apache2'
+    end
+end
+
+service 'www' do
+    case node['platform']
+    when 'centos','redhat','fedora'
+	service_name 'httpd'
+    when 'ubuntu','debian'
+	service_name 'apache2'
+    end
+    supports :restart => true
+    action [ :enable, :start ]
+end
+
+template '/var/www/html/indef.html' do
+    source 'index.html.erb'
+end
